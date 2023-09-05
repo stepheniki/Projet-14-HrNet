7d135047fb8b64b28940d72db71d95fcbf80d597
Stephen Thomas <stepheniki17@gmail.com>
2023-09-05 14:19:39 +0200

    modal

--- front-end-react/src/pages/CreateEmployee.jsx
+++ front-end-react/src/pages/CreateEmployee.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
+import DatePicker from 'react-datepicker'; //selection de date
 import 'react-datepicker/dist/react-datepicker.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { useEmployeeContext } from '../EmployeeContext'; // Importez le contexte
+import statesList from '../data/states.json';
+
 
 function Home() {
   const navigate = useNavigate();
@@ -45,28 +47,28 @@ function Home() {
   };
 
   const handleSaveEmployee = () => {
-    const { street, city, state, zipCode, ...rest } = employeeData; // Extraire les champs de l'adresse
-    const newEmployee = { ...rest, street, city, state, zipCode }; // Inclure les nouveaux champs
-    addEmployee(newEmployee); // Ajoutez le nouvel employé en utilisant le contexte
+    const { street, city, state, zipCode, ...rest } = employeeData;
+    const newEmployee = { ...rest, street, city, state, zipCode };
+    addEmployee(newEmployee);
   
     setEmployees([...employees, newEmployee]);
+  
     // Réinitialisez les données de l'employé après l'ajout
     setEmployeeData({
       firstName: '',
       lastName: '',
       dateOfBirth: null,
       startDate: null,
-      department: 'Sales',
-      street: '', // Réinitialisez les champs de l'adresse
+      department: 'Sales', // Réinitialisez le département à sa valeur par défaut
+      street: '',
       city: '',
       state: '',
       zipCode: '',
     });
   
-  
     // Utilisez navigate pour naviguer vers la page EmployeeList avec les données des employés
     navigate('/employee-list', { state: { employees: [...employees, newEmployee] } });
-  }
+  };
 
   return (
     <>
@@ -74,10 +76,12 @@ function Home() {
         <h1>HRnet</h1>
       </div>
       <div className="container">
-        <Link to="/employee-list">View Current Employees</Link>
+        <button className='button-click'><Link to="/employee-list">
+ View Current Employees</Link></button>
 
-        <h2>Create Employee</h2>
+        
         <form className="form" action="#" id="create-employee">
+        <h2>Create Employee</h2>
           <label htmlFor="first-name">First Name</label>
           <input
             type="text"
@@ -114,32 +118,69 @@ function Home() {
             <legend>Address</legend>
     
             <label htmlFor="street">Street</label>
-            <input id="street" type="text" />
-    
-            <label htmlFor="city">City</label>
-            <input id="city" type="text" />
-    
-            <label htmlFor="state">State</label>
-            <select name="state" id="state"></select>
-    
-            <label htmlFor="zip-code">Zip Code</label>
-            <input id="zip-code" type="number" />
+  <input
+    id="street"
+    type="text"
+    name="street"
+    value={employeeData.street}
+    onChange={handleInputChange}
+  />
+
+  <label htmlFor="city">City</label>
+  <input
+    id="city"
+    type="text"
+    name="city"
+    value={employeeData.city}
+    onChange={handleInputChange}
+  />
+
+<label className='bold' htmlFor="state">State</label>
+<select
+  name="state"
+  id="state"
+  value={employeeData.state}
+  onChange={handleInputChange}
+>
+  <option value="">Select State</option>
+  {statesList.map((state) => (
+    <option key={state.abbreviation} value={state.abbreviation}>
+      {state.name}
+    </option>
+  ))}
+</select>
+
+  <label htmlFor="zip-code">Zip Code</label>
+  <input
+    id="zip-code"
+    type="text"
+    name="zipCode"
+    value={employeeData.zipCode}
+    onChange={handleInputChange}
+  />
           </fieldset>
     
-          <label htmlFor="department">Department</label>
-          <select name="department" id="department">
-            <option>Sales</option>
-            <option>Marketing</option>
-            <option>Engineering</option>
-            <option>Human Resources</option>
-            <option>Legal</option>
-          </select>
-          <button onClick={handleSaveEmployee}>Save</button>
-        </form>
+          <label  htmlFor="department">Department</label>
+<select className='bold'
+  name="department"
+  id="department"
+  value={employeeData.department}
+  onChange={handleInputChange}
+>
+  <option value="Sales">Sales</option>
+  <option value="Marketing">Marketing</option>
+  <option value="Engineering">Engineering</option>
+  <option value="Human Resources">Human Resources</option>
+  <option value="Legal">Legal</option>
+</select>
+</form><br/>
+<button className="button-click" onClick={handleSaveEmployee}>Save</button>
+
       </div>
       <div id="confirmation" className="modal">
         Employee Created!
       </div>
+      
     </>
   );
 }
--- front-end-react/src/pages/EmployeeList.jsx
+++ front-end-react/src/pages/EmployeeList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
+
 function formatDateString(date) {
   if (date) {
     return new Date(date).toLocaleDateString();
@@ -15,37 +16,38 @@ function EmployeeList() {
   return (
     <div id="employee-div" className="container">
       <h1>Current Employees</h1>
-      <table id="employee-table" className="display">
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Date of Birth</th>
-            <th>Start Date</th>
-            <th>Department</th>
-            <th>Street</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Zip Code</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map((employee, index) => (
-            <tr key={index}>
-              <td>{employee.firstName}</td>
-              <td>{employee.lastName}</td>
-              <td>{formatDateString(employee.dateOfBirth)}</td>
-              <td>{formatDateString(employee.startDate)}</td>
-              <td>{employee.department}</td>
-              <td>{employee.street}</td>
-              <td>{employee.city}</td>
-              <td>{employee.state}</td>
-              <td>{employee.zipCode}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <Link to="/">Home</Link>
+      <table id="employee-table" className="table table-bordered table-striped table-hover">
+  <thead>
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Start Date</th>
+      <th>Department</th>
+      <th>Date of Birth</th>
+      <th>Street</th>
+      <th>City</th>
+      <th>State</th>
+      <th>Zip Code</th>
+    </tr>
+  </thead>
+  <tbody>
+    {employees.map((employee, index) => (
+      <tr key={index}>
+        <td>{employee.firstName}</td>
+        <td>{employee.lastName}</td>
+        <td>{formatDateString(employee.startDate)}</td>
+        <td>{employee.department}</td>
+        <td>{formatDateString(employee.dateOfBirth)}</td>
+        <td>{employee.street}</td>
+        <td>{employee.city}</td>
+        <td>{employee.state}</td>
+        <td>{employee.zipCode}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+<br/>
+      <button className='button-click'><Link to="/">Home</Link></button>
     </div>
   );
 }
